12d8dbb693f61e2766db49a76fa53717a667058f
白糖の正义铃 <31569794+atonyxu@users.noreply.github.com>
2023-12-21 22:47:25 +0800

    Add files via upload

---
+++ SV/VCVtoCV.js
@@ -0,0 +1,61 @@
+var SVM = {
+    "cate": "Note Manipulation"
+}
+
+function getClientInfo() {
+    return {
+        "name": SV.T("Japanese VCV to CV"),
+        "category": SV.T(SVM.cate),
+        "author": "白糖の正义铃 BiliBili HomePage: space.bilibili.com/180668218",
+        "versionNumber": 1.1,
+        "minEditorVersion": 65537
+    };
+}
+
+function getTranslations(langCode) {
+    if (langCode == "zh-cn") {
+        return [
+            ["Japanese VCV to CV", "日语VCV转CV"],
+            [SVM.cate, "音符操作"],
+        ];
+    }
+    return [];
+}
+
+function main() {
+    // 获取当前组和选中内容
+    var selection = SV.getMainEditor().getSelection();
+    var selectedNotes = selection.getSelectedNotes();
+    var scope = SV.getMainEditor().getCurrentGroup();
+    var group = scope.getTarget();
+
+    // 获取光标
+    var playhead = SV.getPlayback().getPlayhead();
+    var timeAxis = SV.getProject().getTimeAxis();
+    var playheadBlicks = timeAxis.getBlickFromSeconds(playhead) - scope.getTimeOffset();
+
+    if (selectedNotes.length > 0) {
+        for (var index = 0; index < selectedNotes.length; index++) {
+            var thisNote = selectedNotes[index];
+            var thisLyric = thisNote.getLyrics();
+            if (thisLyric.indexOf("_") > -1) {
+                group.removeNote(thisNote.getIndexInParent());
+            } else if (thisLyric.length > 2 && thisLyric[1] == " ") {
+                thisNote.setLyrics(thisLyric.slice(2));
+            }
+        }
+    } else {
+        var groupNoteNum = group.getNumNotes();
+        for (var i = 0; i < groupNoteNum; i++) {
+            var thisNote = group.getNote(i);
+            var thisLyric = thisNote.getLyrics();
+            if (thisLyric.indexOf("_") > -1) {
+                thisNote.setLyrics("br");
+            } else if (thisLyric.length > 2 && thisLyric[1] == " ") {
+                thisNote.setLyrics(thisLyric.slice(2));
+            }
+        }
+    }
+
+    SV.finish();
+}
